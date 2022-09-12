ac0d7b9412286e84cfc101715c08f41d8d050859
vaibhavsharma <vaibhavsharma3070@gmail.com>
2022-09-12 14:46:18 +0530

    Done:User activation/deactivation.

--- src/PrivateRoute/PrivateRoute.js
+++ src/PrivateRoute/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom"
 
 export const PrivateRoute = () => {
     const token = localStorage.getItem('token');
-    return token ? <Outlet /> : <Navigate to="/Login" />
+    return token ? <Outlet /> : <Navigate to="/" />
 }
 
 
